7d01b469e7df69dfbc0cedbfd61e7ee023180b6f
eneskarateke <eneskarateke@gmail.com>
2023-06-07 16:25:50 +0300

    latest build

---
+++ build/precache-manifest.ec31b10d77e2e0a242c53741bca2484f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87bd94f1657b8a854949",
+    "url": "/static/css/main.8be9282a.chunk.css"
+  },
+  {
+    "revision": "87bd94f1657b8a854949",
+    "url": "/static/js/main.56960457.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f1096916039bc0b1b76b",
+    "url": "/static/css/2.8f9af938.chunk.css"
+  },
+  {
+    "revision": "f1096916039bc0b1b76b",
+    "url": "/static/js/2.e7ba55eb.chunk.js"
+  },
+  {
+    "revision": "472cb07eca7fb0d618d994dd8d7a1707",
+    "url": "/index.html"
+  }
+];
